a63b2a99e3c4d6f02f681a85fc7f85c817ca61b4
Hector Virgen <hvirgen@attinteractive.com>
2011-10-05 13:36:14 -0700

    Adds support for downloading files. Switches to haml for templates.

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
+# Before filter
+before do
+  # Configuration
+  @media_path = File.expand_path(File.dirname(__FILE__) + '/media')
+  
+  # No editing!
+  request.path_info = request.path_info.gsub(/\/+$/, '')
+  @abs_path = "#{@media_path}#{request.path_info}"
+end
+
 # About
 get '/about' do
   erb :about
@@ -7,5 +17,40 @@ end
 
 # Folders and files
 get '/*' do |path|
-  path
+  unless File.exists?(@abs_path)
+    return "#{@abs_path} does not exist!"
+  end
+  
+  if (File.directory?(@abs_path)) then
+    # Show directory contents
+    contents = []
+    Dir.foreach(@abs_path) do |x|
+      # Skip current directory
+      next if (x == '.')
+    
+      # Skip parent directory if at top
+      next if (x == '..' && @abs_path == @media_path)
+    
+      if (x == '..') then
+        contents << {
+          :url => '/' + File.dirname(path).gsub(/^\.$/, ''),
+          :abs_path => File.basename(@abs_path),
+          :name => '.. Parent directory',
+          :rel => 'parent'
+        }
+      else
+        abs_path = "#{@abs_path}/#{x}"
+        contents << {
+          :url => "/" + "#{path}/#{x}".gsub(/^\/*/, ''),
+          :abs_path => abs_path,
+          :name => File.basename(abs_path),
+          :rel => (File.directory?("#{@abs_path}/#{x}") ? 'directory' : 'file')
+        }
+      end
+    end
+
+    haml :directory, :locals => {:contents => contents}
+  else
+    send_file @abs_path
+  end
 end
